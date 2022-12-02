fbd502a0e7a0abb4a6c446d17cda1fe659238563
Vivek Parashar <96679102+Vvivek-Pparashar@users.noreply.github.com>
2022-12-03 00:27:41 +0530

    page2-fully responsive

--- src/Components/Page2.js
+++ src/Components/Page2.js
@@ -14,19 +14,15 @@ const Page2 = () => {
         <Col
           xs={{
             span: 24,
-            order: 2,
           }}
           sm={{
             span: 24,
-            order: 2,
           }}
           md={{
             span: 10,
-            order: 1,
           }}
           lg={{
             span: 10,
-            order: 1,
           }}
         >
           <div className="page2_left">
@@ -41,51 +37,42 @@ const Page2 = () => {
         <Col
           xs={{
             span: 24,
-            order: 1,
           }}
           sm={{
             span: 24,
-            order: 1,
           }}
           md={{
             span: 10,
-            order: 2,
           }}
           lg={{
             span: 10,
-            order: 2,
+
             offset: 4,
           }}
         >
-          <h5
-            style={{
-              fontSize: "2.7vw",
-              color: "#7A553B",
-              marginBottom: "20px",
-              marginTop: "1.7vw",
-            }}
-          >
+          <h5 className="h5_page5">
             We believe in promoting <br /> innovative strategies, a <br />
             multifaceted platfrom <br />
             allowing user to
             <br /> review the startups and <br />
-            get insight about creative discipline for a variety <br />
+            get insight about creative <br />
+            discipline for a variety <br />
             of projects.
           </h5>
 
-          <b style={{ cursor: "pointer", color: "#142C64" }}>About Us</b>
+          <b style={{ cursor: "pointer", color: "#142C64", marginLeft: "2px" }}>
+            About Us
+          </b>
           <div className="arrow_holder">
             <MinusOutlined
               className="minusSign_style"
               style={{
-                marginLeft: "0",
+                marginLeft: "2px",
               }}
             />
             <MinusOutlined className="minusSign_style" />
             <MinusOutlined className="minusSign_style" />
-            <CaretRightOutlined
-              style={{ fontSize: "20px", marginLeft: "-10px" }}
-            />
+            <CaretRightOutlined className="traingleSign_style" />
           </div>
         </Col>
       </Row>
